b65614a9fc783c7c38a09652e7eb2a5f895cb553
Yosinan <ymymym4634@gmial.com>
2022-09-08 13:56:03 -0700

    new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
-#include <stdio.h> 
-  
- /** 
-  * main - Entry point 
-  * 
-  * Return: Always 0 (success) 
-  */ 
- int main(void) 
- { 
-         printf("%s\n", "with proper grammar, but the outcome is a piece of art,"); 
-         return (0); 
- }
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammer, but the outcome is a piece of art,");
+       return(0);
+}       
