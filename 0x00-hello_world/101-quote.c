4c3bd2c33e19f2e820851028257a217b54f5d815
Yosinan <ymymym4634@gmial.com>
2022-09-08 13:35:19 -0700

    new

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,10 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (success)
  */
-int main(void)
-{
-	write(STDOUT_FILENO,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return(1);
+int main(void)                                                        {                                                                             write(STDOUT_FILENO,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+        return(1);
 }
