57478caa5977fe3b57f80506be9160b889ee69b0
Yosinan <ymymym4634@gmial.com>
2022-09-09 09:37:28 -0700

    new

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,7 +9,6 @@
 int main(void)
 {
         int i;
-
         for (i = 97 ; i <= 122 ; i++)
                 putchar(i);
         for (i = 65 ; i <= 90 ; i++)
@@ -18,4 +17,3 @@ int main(void)
 
         return (0);
 }
-
