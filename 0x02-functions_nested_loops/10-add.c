205f4ab5d1ea40e9dcbadd1fb27990acbafc2d94
Yosinan <ymymym4634@gmial.com>
2022-09-13 10:55:25 -0700

    new

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
- * times_table - prints the times table from 0 - 9.
-(*
- * Return: a + b.
+ * add - Add two integers
+ * @x: int type number
+ * @y: int type number
+ * Return: result of addition
  */
-int add(int a, int b)
+int add(int x, int y)
 {
-	return (a + b);
+	return (x + y);
 }
