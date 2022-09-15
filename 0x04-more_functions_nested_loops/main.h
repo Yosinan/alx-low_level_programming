09c4c491d544a1cf4775aea35b66376be3309bc8
Yosinan <ymymym4634@gmial.com>
2022-09-15 12:48:06 -0700

    new

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#ifdefine MAIN_H
-#define MAIN_H
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -11,4 +9,3 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-#endif
