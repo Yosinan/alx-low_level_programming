4134ffc53f8e8898ef2561bdc4ee56dd577f9c96
Yosinan <ymymym4634@gmial.com>
2022-09-15 12:25:04 -0700

    new

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/** 
+ * print_diagonal() - print n number of \
+ *
+ * it has no return type
+ */
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
