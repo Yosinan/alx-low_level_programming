c748c324f19f10055f0352469080d2135ce21e90
Yosinan <ymymym4634@gmial.com>
2022-09-16 02:49:49 -0700

    new

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
-*print_numbers- a function that prints the numbers, from 0 to 9
-*
-*Return: print to stdout the alphabet, in lowercase, followed by a new line.
-**/
+ * print_numbers() - is used to print numbers from zero to nine 
+ * it has a void return type
+ * it doesnt return anything
+ */
 void print_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
-		 putchar(i + '0');
-	 putchar('\n');
+		putchar(i + '0');
+	putchar('\n');
 }
