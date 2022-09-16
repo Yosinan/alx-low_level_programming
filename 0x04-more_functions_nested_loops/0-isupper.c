f54834e039b120aefd73ba4bd92f319d72231f18
Yosinan <ymymym4634@gmial.com>
2022-09-16 03:40:58 -0700

    new

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 /**
  * _isupper - returns 1 if uppercase alphabet and 0 otherwise
  * @c; takes in a character
- * Return: 0 for lowercase, 1 for uppercase
+ * Return: 1 for lowercase, 0 for uppercase
  **/
 int _isupper(int c)
 {
