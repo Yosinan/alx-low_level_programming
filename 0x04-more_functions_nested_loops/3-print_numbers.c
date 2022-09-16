c1893e717c1ee4a82b767cde47cd1354b7833eda
Yosinan <ymymym4634@gmial.com>
2022-09-16 02:41:58 -0700

    new

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,27 +1,15 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- *
- *  * print_numbers() - is used to print numbers from zero to nine 
- *
- *   * it has a void return type
- *
- *    * it doesnt return anything
- *
- *     */
-
+ * print_numbers() - is used to print numbers from zero to nine 
+ * it has a void return type
+ * it doesnt return anything
+ */
 void print_numbers(void)
-
 {
+	int i;
 
-		int i;
-
-
-
-			for (i = 0; i <= 9; i++)
-
-						_putchar(i + '0');
-
-				_putchar('\n');
-
+	for (i = 0; i <= 9; i++)
+		_putchar(i + '0');
+	_putchar('\n');
 }
