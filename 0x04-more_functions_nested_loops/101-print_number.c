7b6d09acebb1fca56e9efaac4a7ac7d5c24f721e
Yosinan <ymymym4634@gmial.com>
2022-09-16 04:00:37 -0700

    new

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,38 +1,40 @@
 #include <stdio.h>
 #include "main.h"
+#include "holberton.h"
 /**
- *print_number - prints an integer
- *
- *@n:input
- *
- *Return:void
- *
- *Description:prints an integer
- *
+ * print_number - prints a number
+ * @n: Input number
  */
+
 void print_number(int n)
 {
-	unsigned int x, y, count;
+	int len, res, i, temp, expo;
 
-	if (n < 0)
+	res = n;
+	expo = len =  1;
+/*Check negatives*/
+	if (res < 0)
 	{
-		_putchar(45);
-		x = -n;
+		res *= -1;
+		_putchar('-');
 	}
-	else
-	{
-		x = n;
-	}
-	y = x;
-	count = 1;
 
-	while (y > 9)
+/**/
+	temp = res;
+	while (temp >= 10)
 	{
-		y /= 10;
-		count *= 10;
+		len++;
+		temp /= 10;
 	}
-	for (; count >= 1; count /= 10)
+
+/*Create Exponent*/
+	for (i = 1; i < len; i++)
+		expo *= 10;
+/*Main */
+	while (expo > 1)
 	{
-		_putchar(((x / count) % 10) + 48);
+		_putchar((res / expo) % 10 + '0');
+		expo /= 10;
 	}
+	_putchar(res % 10 + '0');
 }
