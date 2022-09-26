880024c8ef4d4b9ddb7bef189654a0cd32039d0c
Yosinan <ymymym4634@gmial.com>
2022-09-26 11:24:59 -0700

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,14 +6,14 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int l;
+	int i;
 	int j;
 
-	for (l = 0; l < 8; l++)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
-			_putchar(a[l][j]);
+			_putchar(a[i][j]);
 		}
 		_putchar('\n');
 	}
