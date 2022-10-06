5c115f310a772cee4f9d9c6c5dfb196e234221f5
Yosinan <ymymym4634@gmial.com>
2022-10-06 13:04:41 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - returns pointer which memory is allocated
+ * @b: input
+ * Return: output
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p = (void *)malloc(b);
+
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
