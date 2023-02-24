2048aa1a11a54eb7f24e2c5693468c8276e2e601
Yoseph <ymymym4634@gmail.com>
2023-02-24 21:16:49 +0300

    C-makefile

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -23,6 +23,7 @@ def island_perimeter(grid):
     """Returns the perimeter of the island in grid."""
 
     perimeter = 0
+    
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j]:
