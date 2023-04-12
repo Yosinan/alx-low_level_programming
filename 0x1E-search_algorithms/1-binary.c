ca215a1c9daaaedb3c831c0a679a74cac8338e5d
Yosinan <ymymym4634@gmial.com>
2023-04-12 14:00:39 -0700

    C - Search Algorithms

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -28,21 +28,17 @@ int recurr_fun(int *arr, size_t lf, size_t ryt, int val)
 	while (lf <= ryt) 
 	{
 		printf("Searching in array: ");
-		for (i = lf; i <= ryt; i++) 
-		{
-			printf("%d", array[i]);
-			if (i != end) {
-				printf(", ");
-			else
-				printf("\n");
-		}
+		for (i = lf; i < ryt; i++) 
+		
+			printf("%d, ", array[i]);
+		printf("%d\n", arr[i]);
 
 		i = (lf + ryt) / 2;
 
-		if (array[i] == value) 
+		if (arr[i] == val) 
 			return (i);
 
-		if (array[i] < value )
+		if (arr[i] < val)
 			lf = i + 1;
 		else
 			ryt = i - 1;
