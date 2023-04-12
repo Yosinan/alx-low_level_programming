73a9e3dcda1e857d5f779125fb818f2bd2dbffe1
Yosinan <ymymym4634@gmial.com>
2023-04-12 12:17:20 -0700

    C - Search Algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,5 +1,11 @@
 #include "search_algos.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+
 int linear_search(int *array, size_t size, int value){
 	if (array != NULL) {
 		int idx = 0;
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,5 +1,11 @@
 #include "search_algos.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+
 int recurr_fun(int *arr, size_t lf, size_t ryt, int val);
 
 int binary_search(int *array, size_t size, int value) 
@@ -13,4 +19,28 @@ int binary_search(int *array, size_t size, int value)
 
 int recurr_fun(int *arr, size_t lf, size_t ryt, int val)
 {
-	size_t 
+	while (lf <= ryt) 
+	{
+		printf("Searching in array: ");
+		for (int i = lf; i <= ryt; i++ ) 
+		{
+			printf("%d", array[i]);
+			if (i != end) {
+				printf(", ");
+			else
+				printf("\n");
+		}
+
+		mid = (lf + ryt) / 2;
+
+		if (array[mid] == value) 
+			return (mid);
+
+		if (array[mid] < value )
+			lf = mid + 1;
+		else
+			ryt = mid - 1;
+	}
+		
+	return (-1);
+}
