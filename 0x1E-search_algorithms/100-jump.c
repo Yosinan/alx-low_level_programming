2e20eef422571b6814795ea06ce9893b7306c4b9
Yosinan <ymymym4634@gmial.com>
2023-04-15 06:54:26 -0700

    C - Search Algorithms

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -43,6 +43,8 @@ int jump_search(int *array, size_t size, int value)
 	printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
 
 	if (array[prev] == value)
+	{
 		return (prev);
+	}
 	return (-1);
 }
