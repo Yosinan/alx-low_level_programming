34fe091a8d63bca150e01c221a46b1422235d569
Yosinan <ymymym4634@gmial.com>
2023-04-15 07:46:42 -0700

    C - Search Algorithms

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -3,11 +3,11 @@
 /**
   * jump_search - Searches for a value in a sorted array
   *               of integers using jump search.
-  * @array: A pointer to the first element of the array to search.
-  * @size: The number of elements in the array.
-  * @value: The value to search for.
+  * @array: pointer to the first element of the array to search.
+  * @size: the number of elements in the array.
+  * @value: the value to search for.
   *
-  * Return: If the value is not present or the array is NULL, -1.
+  * Return: if the value is not present or the array is NULL, -1.
   *         Otherwise, the first index where the value is located.
   *
   * Description: Prints a value every time it is compared in the array.
