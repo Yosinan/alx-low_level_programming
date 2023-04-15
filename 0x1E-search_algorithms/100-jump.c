7d7bbdbbac0021c2e961d628d17dbcdc53987a24
Yosinan <ymymym4634@gmial.com>
2023-04-15 06:52:17 -0700

    C - Search Algorithms

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -18,7 +18,7 @@ int jump_search(int *array, size_t size, int value)
 	size_t prev, jump = 0;
 	size_t step;
 
-	if (array == NULL || size == 0)
+	if (array == NULL)
 		return (-1);
 	step = sqrt(size);
 	while (array[jump] < value && jump < size)
