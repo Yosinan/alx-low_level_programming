d38452cefd6bd5f88b403041e4b9ca3995f7359b
Yosinan <ymymym4634@gmial.com>
2023-04-15 01:43:22 -0700

    C - Search Algorithms

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -14,14 +14,28 @@
   */
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t mid;
-	size_t lf = 0;
-	size_t ryt = size - 1;
-
 	if (array == NULL)
 		return (-1);
+	return (adv_bin(array, 0, size - 1, value));
+}
+/**
+  * adv_bin - Searches for a value in a sorted array
+  *                 of integers using binary search.
+  * @array: A pointer to the first element of the array to search.
+  * @lf: The first index
+  * @ryt: the last index
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the index where the value is located.
+  *
+  * Description: Prints the sub-array being searched after each change.
+  */
+int adv_bin(int *array, size_t lf, size_t ryt, int value)
+{
+	size_t mid;
 
-	while (lf <= ryt)
+	if (lf <= ryt)
 	{
 		printf("Searching in array: ");
 		for (mid = lf; mid < ryt; mid++)
@@ -32,9 +46,9 @@ int advanced_binary(int *array, size_t size, int value)
 		if (array[mid] == value && (mid == lf || array[mid - 1] != value))
 			return (mid);
 		if (array[mid] >= value)
-			ryt = mid - 1;
+			return (adv_bin(array, lf, mid - 1, value));
 		else
-			lf = mid + 1;
+			return (adv_bin(array, mid + 1, ryt, value));
 	}
 	return (-1);
-}
+	}
